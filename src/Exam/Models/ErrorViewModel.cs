db0a0379943a124dec29a1bffe75401bc8ce64bd
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-04-27 14:28:25 +0200

    Testowa zmiana

--- src/Exam/Models/ErrorViewModel.cs
+++ src/Exam/Models/ErrorViewModel.cs
@@ -5,7 +5,6 @@ namespace Exam.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId); 
     }
 }
