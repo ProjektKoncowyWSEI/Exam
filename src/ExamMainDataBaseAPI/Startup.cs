f5d86e390a95c850a23fabdef7f8cfa81dc27f3c
Lukasz Nowak <Lukasz.d.nowak@interia.pl>
2019-05-15 22:47:52 +0200

    Dodanie w api QuestionsController

--- src/ExamMainDataBaseAPI/Startup.cs
+++ src/ExamMainDataBaseAPI/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExamMainDataBaseAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,8 @@ namespace ExamMainDataBaseAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=ExamQuestionsDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<ExamQuestionsDbContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
