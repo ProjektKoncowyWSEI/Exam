b993e1d6fd7a344a828117d335601d4afd34f6db
Lukasz Nowak <Lukasz.d.nowak@interia.pl>
2019-05-28 17:48:47 +0200

    Utworzenie generycznego repo

---
+++ src/ExamMainDataBaseAPI/DAL/Repository.cs
@@ -0,0 +1,55 @@
+﻿using ExamMainDataBaseAPI.DAL.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ExamMainDataBaseAPI.DAL
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity:class 
+    {
+        protected DbContext context;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            context.Set<TEntity>().AddRange(entities);
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return context.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(int id)
+        {
+            return context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return context.Set<TEntity>().ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            context.Set<TEntity>().RemoveRange(entities);
+        }
+    }
+}
