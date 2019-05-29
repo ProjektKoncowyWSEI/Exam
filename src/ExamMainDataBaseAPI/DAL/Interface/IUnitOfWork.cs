9eb56e2203f965eac67153e679a753d371742e51
Lukasz Nowak <Lukasz.d.nowak@interia.pl>
2019-05-29 08:03:27 +0200

    ciąg dalszy repo

---
+++ src/ExamMainDataBaseAPI/DAL/Interface/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamMainDataBaseAPI.DAL.Interface
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IQuestionsRep Questions { get; }
+        IAnswersRep Answers { get; }
+        int Complete();
+    }
+}
