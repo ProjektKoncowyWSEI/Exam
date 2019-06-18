dccbd3c652adbe6eaa112fd27459cc0083d14931
Ewelala <ewelina.molenda@outlook.com>
2019-06-18 21:57:25 +0200

    Tłumaczenie - kolejne zmiany

--- src/Exam/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/Exam/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -30,14 +30,15 @@ namespace Exam.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                return Page();
-            }
+            //if (returnUrl != null)
+            //{
+            //    return LocalRedirect(returnUrl);
+            //}
+            //else
+            //{
+            //    return Page();
+            //}
+            return Page();
         }
     }
 }
