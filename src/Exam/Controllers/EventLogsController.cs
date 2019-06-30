36911703d35c92ee55d0b8fd5be1d30d90afea4a
Lukasz Nowak <Lukasz.d.nowak@interia.pl>
2019-06-30 15:05:47 +0200

    Wstępny controller plus widoki do obsługi błędów

---
+++ src/Exam/Controllers/EventLogsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Logger.Data;
+using Logger.Model;
+
+namespace Exam.Controllers
+{
+    public class EventLogsController : Controller
+    {
+        private readonly LoggerDbContext _context;
+
+        public EventLogsController(LoggerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: EventLogs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.EventLog.ToListAsync());
+        }
+
+        // GET: EventLogs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventLog = await _context.EventLog
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (eventLog == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventLog);
+        }
+        // GET: EventLogs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventLog = await _context.EventLog
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (eventLog == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventLog);
+        }
+
+        // POST: EventLogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var eventLog = await _context.EventLog.FindAsync(id);
+            _context.EventLog.Remove(eventLog);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventLogExists(int id)
+        {
+            return _context.EventLog.Any(e => e.id == id);
+        }
+    }
+}
