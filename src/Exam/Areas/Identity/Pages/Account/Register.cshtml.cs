191f178c0025ded37f1003860878ba2d9789c7c4
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-07-07 20:07:32 +0200

    Drobne poprawki w tłumaczeniu i ustawieniach

--- src/Exam/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Exam/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -64,7 +64,7 @@ namespace Exam.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-
+            [Display(Name = "Role")]
             public RoleEnum Role { get; set; } 
         }
 
--- src/Helpers/RoleEnum.cs
+++ src/Helpers/RoleEnum.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Helpers
 {
     public enum RoleEnum
     {
+        [Display(Name = "Lack")]
         lack = 0,
+        [Display(Name = "Student")]
         student = 8,
+        [Display(Name = "Teacher")]
         teacher = 16,
+        [Display(Name = "Admin")]
         admin = 64        
     }
 }
